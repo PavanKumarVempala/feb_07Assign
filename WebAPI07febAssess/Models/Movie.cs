e13783a5f3176761c587a0f3cb7167ae10e26029
Pavan <vpavankumar1999@gmail.com>
2024-02-07 14:56:15 +0530

    Initial commit.

---
+++ WebAPI07febAssess/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebAPI07febAssess.Models
+{
+    [Table("Movie")]
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        [StringLength(100)]
+        public string MName { get; set; }
+        public string StatCast { get; set; }
+        public string Director { get; set; }
+        public string Producer { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
